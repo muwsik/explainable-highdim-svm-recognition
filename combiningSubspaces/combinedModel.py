mathematically correct averaging and recalculation of b

combiningSubspaces/combinedModel.py
@@ -24,7 +24,7 @@ class combLinModel:
         self.subspaceIndex = np.array_split(inds, self.numSplits)
         self.subspaceModels = []
 
-        # training by subspaces
+        # training by subspaces 
         for tempSubspace in self.subspaceIndex:
             timeStartTrain = time.time()
             tempModel = self.baseModel()
@@ -33,15 +33,26 @@ class combLinModel:
 
             self.subspaceModels.append([tempModel, timeEndTrain - timeStartTrain])
 
-        # combining general solution 
+        # combining general solution as an average of subspace particular models
         for tempModel, _ in self.subspaceModels:
-            self.a = np.hstack((self.a, tempModel.coef_[0]))
-            self.b += tempModel.intercept_[0]
+            # normalization of the subspace model to preserve scale
+            tempNorm = np.linalg.norm(tempModel.coef_[0])
+            tempNormA = tempModel.coef_[0] / tempNorm
+            tempNormB = tempModel.intercept_[0] / tempNorm
 
-        self.a /= np.linalg.norm(self.a) 
-        self.b /= len(self.subspaceIndex)
+            # since the models are built in orthogonal coordinate systems,
+            # their addition in the final expanded space can be replaced by a simple union
+            self.a = np.hstack((self.a, tempNormA))
 
-        #
+            # displacement is added by the property of linear functions
+            self.b += tempNormB
+
+        # statistical averaging of models -> 1 / √N
+        # algebraic averaging of models -> 1 / N
+        self.a /= np.sqrt(len(self.subspaceIndex))
+        self.b /= np.sqrt(len(self.subspaceIndex))
+
+        # initial order of features
         temp = np.empty_like(self.a)
         temp[np.concatenate(self.subspaceIndex)] = self.a
         self.coef_ = [temp]
@@ -60,43 +71,17 @@ class combLinModel:
         scores = self.decision_function(X)
         labels = np.where(scores >= 0, 1, -1)
         return labels
-    
-
-def computeIntercept(model, X, Y, eps = 1e-3):
-    """
-    Estimate the bias *b* ONLY for LinearSVC using points near the margin.
-        model: trained LinearSVC model
-        X: objects of training sample
-        Y: objects labels in {-1, 1}
-        eps: tolerance for selecting points with *margin = 1 ± eps*
-    Return
-        b: estimated hyperplane displacement
-    """
-
-    # 
-    decision = model.decision_function(X)
-    margin = Y * decision
-
-    # potentially supporting objects
-    mask = np.abs(margin - 1) < eps
-    #print(f"count potentially supporting objects: {np.sum(mask)}")
-
-    b = None
-    if (np.sum(mask) > 0):
-        b = np.mean(Y[mask] - np.dot(X[mask], model.coef_[0]))
-    # else no potentially supporting objects with tolerance *eps*
-    # you can increase the value *eps*
-    
-    return b
 
 
-if __name__ == "__main__":
-    from sklearn.svm import LinearSVC
-    from dataGenerator.sample import Sample
+    def recalucateIntercept(self, X, Y, eps = 1e-2):        
+        inds = np.concatenate(self.subspaceIndex)
+        decision  = np.dot(X[:, inds], self.a)
 
-    trainDataset = Sample.fromBin(r"D:\datasets\b-10k-1k.npz")
+        mask = np.abs(Y*decision - 1) < eps
 
-    model = LinearSVC(penalty = 'l1', dual = False, verbose = True)
-    model.fit(trainDataset.X, trainDataset.Y)
+        b = None
+        if (np.sum(mask) > 0):
+            b = np.mean(Y[mask] - decision[mask])
 
-    b = computeIntercept(model, trainDataset.X, trainDataset.Y)
+        self.b = b
+        return b

combiningSubspaces/experiment.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     parser.add_argument("--output", type = str, required = True,
         help = "Output Excel file")
     parser.add_argument("--model", type = str, required = True,
-        choices=  ["SVC", "LinearSVC", "CombLinSVM-LSVC"],
+        choices=  ["SVC", "LinearSVC", "CombLinSVM-LSVC-l1", "CombLinSVM-LSVC-l2"],
         help = "Model type")    
     
     # 1.2 Base SVM parameters
@@ -66,10 +66,10 @@ if __name__ == "__main__":
         "C": args.C,
 
         # SVC
-        "kernel": check(args.kernel),
+        #"kernel": check(args.kernel),
         
         # LinearSVC
-        "penalty": check(args.penalty),
+        #"penalty": check(args.penalty),
 
         # CombLinSVM
         "splits": check(args.splits)
@@ -101,10 +101,13 @@ if __name__ == "__main__":
     if args.model == "SVC":    
         model = SVC(C = args.C, kernel = args.kernel, verbose = _verbose)
     elif args.model == "LinearSVC":    
-        model = LinearSVC(C = args.C, penalty = args.penalty, dual = False, verbose = _verbose, max_iter = 2000)
-    elif args.model == "CombLinSVM-LSVC":
+        model = LinearSVC(C = args.C, penalty = args.penalty, dual = 'auto', verbose = _verbose, max_iter = 5000)
+    elif args.model == "CombLinSVM-LSVC-l1":
         model = combLinModel(numSplits = args.splits,
             baseModel = lambda: LinearSVC(C = args.C, penalty = 'l1', dual = False, verbose = _verbose))
+    elif args.model == "CombLinSVM-LSVC-l2":
+        model = combLinModel(numSplits = args.splits,
+        baseModel = lambda: LinearSVC(C = args.C, penalty = 'l2', dual = 'auto', verbose = _verbose))
     else:
         raise ValueError("Unknown model")
 
@@ -112,6 +115,9 @@ if __name__ == "__main__":
     print(f"Training model {args.model}")
     timeTrain = -time.time()
     model.fit(trainDataset.X, trainDataset.Y)
+    if args.model == "CombLinSVM-LSVC-l2" or args.model == "CombLinSVM-LSVC-l1":
+        model.recalucateIntercept(trainDataset.X, trainDataset.Y)
+
     timeTrain += time.time()
 
     # 2.5 Predicting
@@ -123,10 +129,11 @@ if __name__ == "__main__":
     # 2.Final 
     results = {
         "acc(test)": accuracy_score(testDataset.Y, myLabels),
-        "cosine": cosine_similarity([trainDataset.params["a"]], [model.coef_[0]])[0][0] if "a" in trainDataset.params else 0,
         "acc(train)": accuracy_score(trainDataset.Y, model.predict(trainDataset.X)),
-        "b": model.intercept_[0],
         "time(train)": timeTrain,
+        #"cosine": cosine_similarity([trainDataset.params["a"]], [model.coef_[0]])[0][0] if "a" in trainDataset.params else 0,
+        "b": model.intercept_[0],
+        "num_nonzero": np.sum(np.abs(model.coef_) > 1e-6),
         #"time(predict)": timePredict,
     }
 